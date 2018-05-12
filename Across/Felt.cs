9de97ba113177f361162db9c4de26bef44f093d2
FilipHojgaard <filip.hojgaard@gmail.com>
2018-05-12 20:46:20 +0200

    Hele spillet færdigt. v1.0. spilbart.

---
+++ Across/Felt.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Data;
+
+namespace Across
+{
+    class Felt
+    {
+        private bool _real;  // Feltets rigtige værdi. er den 0 eller 1? det skal spilleren regne ud. Bliver random af Randomizer()
+        public bool guess;  // spillerens nuværende gæt på feltet
+        public bool correct = false;    // er _real og guess ens? bliver checket i check()
+        public int rows;    // objekterne fra denne klasse bliver lavet som 2d array. dette er i'et fra nested for loop
+        public int cols;    // dette er j'et fra nested for loop
+        public int posX;    // udregnet position X fra rows i metoden Positionize()
+        public int posY;    // udregnet position Y fra cols i metoden Positionize()
+        public int id;      // id udregnet ved rows * 10 + cols
+        PictureBox picture; // picturebox til at tegne front end for objektet
+        Form1 form;          // reference til form så der kan tegnes Picturebox fra denne klasse.
+
+
+        // Constructor. henter parametre og tegner.
+        public Felt(Random rnd, Form1 formArg, int rowArg, int ColArg)
+        {
+            form = formArg;
+            rows = rowArg + 1;
+            cols = ColArg + 1;
+            Randomize(rnd);
+            Positionize(rows, cols);
+            id = rows * 10 + cols;
+            Draw();
+            ID();
+        }
+
+        // Test metode man kan kalde for at vide hvilket felt man har med at gøre.
+        public void ID()
+        {
+            Console.WriteLine("ID: " + id + " position: " + posX + " " + posY);
+        }
+
+        // Metode der tegner picturebox. det med at gange location med 10 var et tilfælde jeg opdagede men det virkede!
+        public void Draw()
+        {
+            picture = new PictureBox {
+                Name = "picturebox",
+                Size = new Size(100, 100),
+                Location = new Point(posX*10, posY*10),
+                Image = Image.FromFile("C:/Users/filip/source/repos/Across/Across/sprites/tom_felt.png"),
+            };
+            form.Controls.Add(picture);
+
+            // "type picture.MouseClick +=" og så TAB og den færdiggør en mouseclick event handler som ses lige under.
+            picture.MouseClick += Picture_MouseClick;
+        }
+
+        // Mouse click event handler. Oprettet med TAB. Læs ovenover.
+        private void Picture_MouseClick(object sender, MouseEventArgs e)
+        {
+            ID();
+            if (e.Button == MouseButtons.Left) {
+                picture.Image = Image.FromFile("C:/Users/filip/source/repos/Across/Across/sprites/felt_1.png");
+                guess = true;
+                Analyze();
+            }
+
+            if (e.Button == MouseButtons.Right) {
+                picture.Image = Image.FromFile("C:/Users/filip/source/repos/Across/Across/sprites/felt_0.png");
+                guess = false;
+                Analyze();
+            }
+            if (e.Button == MouseButtons.Middle) {
+                picture.Image = Image.FromFile("C:/Users/filip/source/repos/Across/Across/sprites/tom_felt.png");
+                guess = false;
+                Analyze();
+            }
+        }
+
+        // Checker om _real og guess er ens og returner enten true eller false og gemmer det også i correct
+        public void Check()
+        {
+            if (_real == guess) {
+                correct = true;
+            } else {
+                correct = false;
+            }
+        }
+
+        // Sætter 50% change om _real er false eller true
+        public void Randomize(Random rnd)
+        {
+            int r = rnd.Next(0, 3);
+            if (r == 0) {
+                _real = false;
+            } else {
+                _real = true;
+            }
+        }
+
+        // Sætter felternes position afhængig af deres id fra rows og cols
+        public void Positionize(int rows, int cols)
+        {
+            posX = rows * 10;
+            posY = cols * 10;
+        }
+
+        // metode skal i stedet skrives i Forms og loope over alle Felt objekterne og tjekke deres correct efter at have kørt Check(). 
+        // Hvis de alle giver true har man vundet spillet.
+        public void Analyze()
+        {
+            // Do checking stuff anytime a button is clicked
+            Check();
+            Console.WriteLine("real: " + _real + " guess: " + guess + " correct: " + correct);
+            form.CheckForWin();
+        }
+
+        // Test metode der viser hvad _real er eftersom den er private.
+        public bool ShowReal()
+        {
+            return _real;
+        }
+
+    }
+}
---
+++ Across/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Across
+{
+    public partial class Form1 : Form
+    {
+        public Random rnd;
+        Felt[,] felt;
+        int count;
+
+        Label rightOne;
+        Label rightTwo;
+        Label rightThree;
+        Label downOne;
+        Label downTwo;
+        Label downThree;
+
+        public Form1()
+        {
+            InitializeComponent();
+            rnd = new Random();
+            Start();
+            MakeLabels();
+        }
+
+        // Laver de forskellige labels, bliver kaldt i starten
+        public void MakeLabels()
+        {
+            rightOne = new Label {
+                Text = "hej",
+                Name = "oppe1",
+                AutoSize = true,
+                Location = new Point(400, 125),
+                Visible = true,
+            };
+            rightTwo = new Label {
+                Text = "hej",
+                Name = "oppe2",
+                AutoSize = true,
+                Location = new Point(400, 225),
+                Visible = true,
+            };
+            rightThree = new Label {
+                Text = "jeg er label 3",
+                Name = "oppe3",
+                AutoSize = true,
+                Location = new Point(400, 325),
+                Visible = true,
+            };
+
+            downOne = new Label {
+                Text = "hej",
+                Name = "oppe1",
+                AutoSize = true,
+                Location = new Point(122, 400),
+                Visible = true,
+            };
+            downTwo = new Label {
+                Text = "hej",
+                Name = "oppe2",
+                AutoSize = true,
+                Location = new Point(222, 400),
+                Visible = true,
+            };
+            downThree = new Label {
+                Text = "hej",
+                Name = "oppe3",
+                AutoSize = true,
+                Location = new Point(322, 400),
+                Visible = true,
+            };
+            this.Controls.Add(rightOne);
+            this.Controls.Add(rightTwo);
+            this.Controls.Add(rightThree);
+
+            this.Controls.Add(downOne);
+            this.Controls.Add(downTwo);
+            this.Controls.Add(downThree);
+
+            if (rightThree.Name == "oppe3") {
+                Console.WriteLine("oppe 3 eksistere" + rightThree.Text);
+            }
+
+            LabelInformation();
+
+        }
+
+        public void LabelInformation()
+        {
+            int oppe1 = 0;
+            int oppe2 = 0;
+            int oppe3 = 0;
+            int nede1 = 0;
+            int nede2 = 0;
+            int nede3 = 0;
+
+            if (felt[0, 0].ShowReal()) {
+                oppe1 += 1;
+                nede1 += 1;
+            }
+            if (felt[1, 0].ShowReal()) {
+                oppe1 += 1;
+                nede2 += 1;
+            }
+            if (felt[2, 0].ShowReal()) {
+                oppe1 += 1;
+                nede3 += 1;
+            }
+
+            if (felt[0, 1].ShowReal()) {
+                oppe2 += 1;
+                nede1 += 1;
+            }
+            if (felt[1, 1].ShowReal()) {
+                oppe2 += 1;
+                nede2 += 1;
+            }
+            if (felt[2, 1].ShowReal()) {
+                oppe2 += 1;
+                nede3 += 1;
+            }
+
+            if (felt[0, 2].ShowReal()) {
+                oppe3 += 1;
+                nede1 += 1;
+            }
+            if (felt[1, 2].ShowReal()) {
+                oppe3 += 1;
+                nede2 += 1;
+            }
+            if (felt[2, 2].ShowReal()) {
+                oppe3 += 1;
+                nede3 += 1;
+            }
+            rightOne.Text = oppe1.ToString();
+            rightTwo.Text = oppe2.ToString();
+            rightThree.Text = oppe3.ToString();
+            downOne.Text = nede1.ToString();
+            downTwo.Text = nede2.ToString();
+            downThree.Text = nede3.ToString();
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("det er startet");
+            felt = new Felt[3, 3];
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    felt[i,j] = new Felt(rnd, this, i, j);
+                }
+            }
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    //felt[i,j].Randomize();
+                    Console.WriteLine("felt " + i + j + " er " + felt[i,j].ShowReal());
+                }
+            }
+        }
+
+            // 1. check() on all felt objects in a loop. 
+            // 2. count all "false" corrects on all the objects
+            // 3. If there is 0 false. YOU WIN
+            // else nothing happens and continue to play
+        public void CheckForWin()
+        {
+            count = 0;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    felt[i, j].Check();
+                    if (felt[i,j].correct == false){
+                        count += 1;
+                    }
+                }
+            }
+            if (count == 0) {
+                MessageBox.Show("You Win!");
+            }
+        }
+    }
+}
